e61d3a7b1650abcc73c655a2e05bde23c6920238
Anthony Rosario <ajrosario08@gmail.com>
2015-10-21 21:26:21 -0700

    added CalculatorBrain.swift file

---
+++ MyCalculator/CalculatorBrain.swift
@@ -0,0 +1,21 @@
+//
+//  CalculatorBrain.swift
+//  MyCalculator
+//
+//  Created by Anthony Rosario on 10/21/15.
+//  Copyright © 2015 Anthony Rosario. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorBrain {
+    
+    enum Op {
+        case Operand
+        case Operation
+    }
+    
+    var opStack = [Op]
+    
+    
+}
